494a3d6d94f495e6f55e1376f820146648ab52a2
Hunthode_Gbenga <ghunthode@gmail.com>
2022-04-12 04:30:11 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - a function that prints 10x the alphabet
+ *
+ * return x10 a-
+ */
+void print_alphabet_x10(void)
+{
+	int n, i;
+
+	i = 0;
+
+	while (i < 10);
+	{
+		for (n = 'a'; n <= 'z'; n++)
+		{
+			_putchar(n);
+		}
+		i++
+		_putchar(*\n*);
+	}
+}
