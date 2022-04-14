32e505e775cb206a5f5086626bc080ff12074cca
Hunthode_Gbenga <ghunthode@gmail.com>
2022-04-14 08:26:42 -0700

    task0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - evalue if letter is  uppercase .
+ * @c: print int
+ * Return: Always 0.
+ */
+
+int _isupperb (int c)
+{
+if (c >= 'A' && c <= 'Z')
+}
+else
+{
+return (0);
+}
+
+}
+:wq
