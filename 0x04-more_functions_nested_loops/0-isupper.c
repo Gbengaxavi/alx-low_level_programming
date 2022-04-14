4c275ca1a78417b89ed1f5b19a708a203e06e75b
Hunthode_Gbenga <ghunthode@gmail.com>
2022-04-14 08:45:36 -0700

    0task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,18 @@
 #include "main.h"
+
 /**
  * _isupper - evalue if letter is  uppercase .
  * @c: print int
  * Return: Always 0.
  */
 
-int _isupperb (int c)
+int _isupper (int c)
 {
 if (c >= 'A' && c <= 'Z')
+{
+return (1);
 }
+
 else
 {
 return (0);
