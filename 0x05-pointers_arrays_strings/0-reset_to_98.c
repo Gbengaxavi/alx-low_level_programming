8bd217ab79bc821ad6e3ca0361a29d31e3d9a2a3
Hunthode_Gbenga <ghunthode@gmail.com>
2022-04-20 05:11:12 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates an integer value to 98
+ *@n: A pointer to an integer that we will update
+ *
+ *Return: void means correct
+ */
+
+void_reset_to_98(int *n)
+
+[
+*n = 98;
+]
